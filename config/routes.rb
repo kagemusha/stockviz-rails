abbe6190e8d1aea50fe653d37bfc564e7d68be38
kagemusha <mmadrid99@gmail.com>
2021-11-23 13:21:40 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :factors
   resources :groups
   resources :categories
-  resources :analyses
+  jsonapi_resources :analyses
+  jsonapi_resources :factors
   jsonapi_resource :profile
   jsonapi_resource :profiles
-  resources :user_profiles
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
